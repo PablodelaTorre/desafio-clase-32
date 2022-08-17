373481a7a9af3b0730211ec410868a81ed7226cb
Pablo <delatorre.pablodaniel@gmail.com>
2022-08-17 14:39:37 -0300

    corrigiendo importeds

--- src/daos/MongoDBMensajes.js
+++ src/daos/MongoDBMensajes.js
@@ -1,4 +1,4 @@
-import MongoClass from "../contenedores/MongoClass.js";
+import MongoClass from "../contenedores/mongoClass.js";
 import { schemaMensajes } from "../models/mensajesSchema.js";
 
 export class MongoDBMensajes extends MongoClass {
