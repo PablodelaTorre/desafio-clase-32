afc94dd6e886e6534913f6d6988bc33a9b30b76c
Pablo <delatorre.pablodaniel@gmail.com>
2022-08-17 23:49:34 -0300

    carritos agregados nuevamente

--- src/daos/index.js
+++ src/daos/index.js
@@ -1,12 +1,16 @@
 import dotenv from 'dotenv';
 dotenv.config();
 let productosDao
+let carritosDao
 let mensajesDao
 
 switch (process.env.DB_CONNECTION) {
     case 'mongoDB':
         import('./MongoDBProductos.js').then(({MongoDBProductos})=>{
             productosDao = new MongoDBProductos();
+        })
+        import("./MongoDBCarritos.js").then(({ MongoDBCarritos }) => {
+            carritosDao = new MongoDBCarritos();
         })
         import('./MongoDBMensajes.js').then(({MongoDBMensajes})=>{
             mensajesDao = new MongoDBMensajes();
