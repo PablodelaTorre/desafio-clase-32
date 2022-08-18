094a5002144c346e71f89a2236f06b96dc90a39f
Pablo <delatorre.pablodaniel@gmail.com>
2022-08-18 01:39:33 -0300

    el envio del mail con sandinblue no esta funcionando

--- src/routes/registro.js
+++ src/routes/registro.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { logConsole } from "../../logger.js";
+import nodemailer from "nodemailer"
 //import passport from "passport";
 
 const router = Router();
 
 export const usuarios = []
 
-router.get("/", (req, res) => {
+router.get("/",async (req, res) => {
     logConsole.info(`${req.url}`)
     logConsole.info(`${req.method}`)
     res.render("./partials/registro");
@@ -16,13 +17,40 @@ router.get("/", (req, res) => {
 //     failureRedirect:'./partials/errorRegistro',
 //     succesRedirect:'./partials/login',
 // }))
-router.post("/", (req, res) => {
+router.post("/",async (req, res) => {
 
     if (usuarios.some(usuario=>usuario.nombre===req.body.nombre)){
         return res.render("./partials/errorRegistro")
     }
-    usuarios.push(req.body)
-    res.render("./partials/login")
+
+    try {
+        usuarios.push(req.body)
+        // res.render("./partials/login")
+        const transporter = nodemailer.createTransport({
+            host: 'smtp-relay.sendinblue.com',
+            port: 587,
+            auth: {
+                user: 'delatorre.pablodaniel@gmail.com',
+                pass: '1Y9IzpG8vgtH34O2'
+            }
+        });
+    
+        await transporter.sendMail({
+            from:"Pablo de la Torre",
+            to:"delatorre.pablodaniel@gmail.com",
+            subject:"Nuevo Registro",
+            html:`<h1>Nuevo Registro</h1>
+                <p>Datos</p>
+                <p>${req.body.email}</p>
+                <p>${req.body.nombre}</p>
+                <p>${req.body.telefono}</p>
+            `,
+        })
+        res.send("Mail enviado con éxito")
+    } catch (error) {
+        res.status(500)
+    }
+    
 });
 
 
