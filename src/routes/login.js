abeed5c471601e42542b8f5205d4d48d0a76f0fe
Pablo <delatorre.pablodaniel@gmail.com>
2022-08-25 12:50:40 -0300

    controllers login agregado

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import { usuarios } from "./registro.js";
 import { logConsole } from "../../logger.js";
+import { loginUsuario } from "../controllers/login.js";
 //import passport from "passport";
 
 const router = Router();
@@ -17,22 +17,8 @@ router.get("/", (req, res) => {
 //     succesRedirect:'./partials/login',
 // }))
 
-router.post("/", (req, res) => {
-    const {nombre, password} = req.body
-    const usuario = usuarios.find(usuario=>usuario.nombre===nombre)
-    if(usuario && usuario.password===password){
-        for (const key in req.body){
-            req.session[key] = req.body[key]
-        }
-        res.redirect('/')
-    } else {
-        res.render('./partials/errorLogin')
-    }
-    
-    // const { nombre } = req.body;
-    // console.log(nombre);
-    // req.session.nombre = nombre;
-    // res.redirect("/");
+router.post("/",async (req, res) => {
+    await loginUsuario(req,res)
 });
 
 
